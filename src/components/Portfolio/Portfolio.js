7d179f0f2514e40c9932d935535d310d1b012eb0
tomcxa <tomcxa@yandex.ru>
2020-05-29 14:10:15 +0300

    update Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,45 +5,45 @@ import projects from '../../projects';
 
 import './index.css';
 
+function getFilters(db) {
+    const categories = new Set();
+
+    db.forEach((project) => {
+        categories.add(project.category);
+    });
+
+    return ['All', ...categories];
+}
+
 const Portfolio = () => {
-    function getFilters(db) {
-        const categories = new Set();
-
-        db.forEach((project) => {
-            categories.add(project.category);
-        });
-
-        return ['All', ...categories];
-    }
 
     const filters = getFilters(projects);
 
     const initialState = {
-        filters,
         selected: 'All',
         projects
     }
+
     const [state, setState] = useState(initialState);
+
+    function onSelectFilterHandle(filter) {
+        if (filter === 'All') {
+            setState(initialState);
+        } else {
+            const filteredProjects = initialState
+                .projects
+                .filter(({category}) => category === filter);
+
+            setState({ selected: filter, projects: filteredProjects });
+        }
+    }
 
     return (
         <div className="portfolio">
             <Toolbar 
-                filters={state.filters}
+                filters={filters}
                 selected={state.selected}
-                onSelectFilter={(filter) => { //правильно ли реализован setState?
-                    setState(() => {
-                        if (filter === 'All') return initialState;
-
-                        const filtered = initialState
-                            .projects
-                            .filter((project) => project.category === filter);
-
-                        return {...initialState,
-                            selected: filter,
-                            projects: filtered
-                        }
-                    });
-                }}
+                onSelectFilter={onSelectFilterHandle}
             />
             <ProjectList projects={state.projects}/>
         </div>
